bd5a5d9cb18473d02dcb6b377f1679506c96bcae
Renan Tinti <renanoliveira@deloitte.com>
2022-12-14 21:08:49 -0300

    custom queries - movie age example

--- api/schema.py
+++ api/schema.py
@@ -10,6 +10,13 @@ class MovieType(DjangoObjectType):
     class Meta:
         model = Movie
 
+    # New attribute created for the MovieType class. This is not an attribute of our Movie model. It can be used only in graphql query
+    movie_age = graphene.String()
+
+    # Resolving the movie_age attribute. It can be queried as "movieAge" in graphql url. Any logic can be added inside this method
+    def resolve_movie_age(self, info):
+        return "Old movie" if self.year < 2000 else "New movieS"
+
 class DirectorType(DjangoObjectType):
     class Meta:
         model = Director
@@ -20,6 +27,25 @@ class Query(graphene.ObjectType):
     # Setting all our movies object to a list of MovieType data. In the graphql interface, it will read "all_movies" as "allMovies"
     all_movies = graphene.List(MovieType)
 
+    # Setting an attribute to query a single object by its id. It is necessary to create a specific resolve method to it too
+    # It is possible to pass another parameters, like the title in the exmaple below
+    movie = graphene.Field(MovieType, id=graphene.Int(), title=graphene.String())
+
     # Method to query our objects the same way we do in our views
     def resolve_all_movies(self, info, **kwargs):
         return Movie.objects.all()
+
+    # Method to query a single object by its id
+    def resolve_movie(self, info, **kwargs):
+
+        # Getting the id from the kwargs. The id needs to be passed as argument in the query in graphql interface
+        id = kwargs.get('id')
+        title = kwargs.get('title')
+
+        if id is not None:
+            return Movie.objects.get(pk=id)
+
+        if title is not None:
+            return Movie.objects.get(title=title)
+
+        return None
